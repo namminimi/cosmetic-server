c0850d1a44b90da7f151ea74d556d02848970ada
namminsub <tbvhdaos@naver.com>
2023-01-25 17:39:41 +0900

    cosmetic-server

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const cors = require("cors");
  //mysql부르기
  const mysql = require("mysql")
 //서버생성 -----> express()호출
-const app = express();
-//프로세서의 주소 포트번호 지정
+const app = express(); // 서버를 만들어줌
+//프로세서의 주소 포트번호 지정 
 const port = 8080;
 
 const multer = require("multer");
 console.log(multer)
 
-//서버의 upload를 클라이언트 접근가능하도록 설정
+//서버의 upload를 클라이언트 접근가능하도록 설정 / 이미지로 접근 가능
 app.use("/upload", express.static("upload"));
 //통신하기위한 데이터포멧 json
 //json 형식의 데이터를 처리할수 있도록 설정
 app.use(express.json()); //use 사용할거다 제이슨 데이터
 //브라우저의 cors이슈를 막기위해 사용하는 코드
 app.use(cors());
-//diskStroage()----------> 파일을 저장할때의 모든 제어 기능을 제공
+//diskStroage()----------> 파일을 저장할때의 모든 제어 기능을 제공 ///multer설정 (multer = 파일 업로드를 담당함)
 const storage = multer.diskStorage({
     destination: (req, file, cb) =>{
         cb(null, 'upload/');     ///함수고 업로드파일이 담긴다 근데 암호화상태로 담김
@@ -71,6 +71,18 @@ app.post("/addProduct",async (req, res) => {
         res.send("ok");
     })
 })
+
+//삭제요청시 처리하기
+app.delete('/delProduct/:id', async (req, res)=> {  //파람스라는 객체에 :id들어감
+    //console.log(req);//요청객체
+    const {id} = req.params;
+    conn.query(`delete from products where p_id=${id}`, (err, rerult, fieelds)=>{
+        res.send("ok")
+    })
+
+})
+
+
 
 app.get("/products/:id", (req, res)=>{
     const params = req.params  //{id: 2}
